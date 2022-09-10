917955662b01960a726304bb423227888ed26fde
Raffer Li <li.raffer0@gmail.com>
2022-09-10 00:43:25 +0000

    Feature/12 sungottamove

--- js/script.js
+++ js/script.js
@@ -1,23 +1,67 @@
-const SUNDAY_BUTTON = document.getElementById('sunday-button');
-const SATURDAY_BUTTON = document.getElementById('saturday-button');
-const SUNDAY_SCHEDULE = document.getElementById('sunday-schedule');
-const SATURDAY_SCHEDULE = document.getElementById('saturday-schedule');
+const SUNDAY_BUTTON = document.getElementById("sunday-button");
+const SATURDAY_BUTTON = document.getElementById("saturday-button");
+const SUNDAY_SCHEDULE = document.getElementById("sunday-schedule");
+const SATURDAY_SCHEDULE = document.getElementById("saturday-schedule");
 
 const makeSaturdayScheduleActive = () => {
-    console.log('Making saturday active')
-    SUNDAY_BUTTON.classList.remove('active');
-    SUNDAY_SCHEDULE.classList.remove('active');
-    SATURDAY_BUTTON.classList.add('active');
-    SATURDAY_SCHEDULE.classList.add('active');
+  console.log("Making saturday active");
+  SUNDAY_BUTTON.classList.remove("active");
+  SUNDAY_SCHEDULE.classList.remove("active");
+  SATURDAY_BUTTON.classList.add("active");
+  SATURDAY_SCHEDULE.classList.add("active");
 };
 
 const makeSundayScheduleActive = () => {
-    console.log('Making sunday active')
-    SUNDAY_BUTTON.classList.add('active');
-    SUNDAY_SCHEDULE.classList.add('active');
-    SATURDAY_BUTTON.classList.remove('active');
-    SATURDAY_SCHEDULE.classList.remove('active');
+  console.log("Making sunday active");
+  SUNDAY_BUTTON.classList.add("active");
+  SUNDAY_SCHEDULE.classList.add("active");
+  SATURDAY_BUTTON.classList.remove("active");
+  SATURDAY_SCHEDULE.classList.remove("active");
 };
 
 SUNDAY_BUTTON.onclick = makeSundayScheduleActive;
 SATURDAY_BUTTON.onclick = makeSaturdayScheduleActive;
+
+var sunRef = document.getElementById("Sun");
+var scrollpercentOriginal = 0;
+function getScrollPercent() {
+  var h = document.documentElement,
+    b = document.body,
+    st = "scrollTop",
+    sh = "scrollHeight";
+  return ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100;
+}
+
+window.addEventListener("scroll", () => {
+  scrollPercent = getScrollPercent();
+
+  console.log(scrollPercent);
+  scrollpercentOriginal = scrollPercent;
+
+  if (window.innerWidth > 800) {
+    if (scrollPercent < 10) {
+      sunRef.style.top = scrollPercent * 3 + "vw";
+      sunRef.style.left = "12vw";
+    } else if (scrollPercent < 16) {
+      sunRef.style.top = scrollPercent * 4 + "vw";
+      sunRef.style.left = scrollPercent * 3 + "vw";
+    } else if (scrollPercent < 20) {
+      sunRef.style.top = scrollPercent * 5 + "vw";
+      sunRef.style.left = scrollPercent * 2 + "vw";
+    } else if (scrollPercent < 30) {
+      sunRef.style.top = scrollPercent * 5 + "vw";
+      sunRef.style.left = scrollPercent + "vw";
+    } else {
+      sunRef.style.top = scrollPercent * 4.3 + "vw";
+    }
+  } else {
+    sunRef.style.left = "50vw";
+    if (scrollPercent < 10) {
+      sunRef.style.top = scrollPercent * 5 + "vw";
+    } else if (scrollPercent < 16) {
+      sunRef.style.top = scrollPercent * 6 + "vw";
+    } else {
+      sunRef.style.top = scrollPercent * 4.3 + "vw";
+    }
+  }
+});
