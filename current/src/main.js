790565c225fee01817989a6b1ba82fb8253f43b3
khang <khang.h.t@utexas.edu>
2024-09-13 17:02:27 -0500

    Restructure folder, merge all css into one

--- current/src/main.js
+++ current/src/main.js
@@ -1,3 +1,4 @@
+import './style.css';
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RectAreaLightUniformsLib } from "three/addons/lights/RectAreaLightUniformsLib.js";
