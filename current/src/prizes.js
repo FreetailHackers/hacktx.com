7074428defc30be36ae2dc7f48cab7ae4c0af7d2
khang <khang.h.t@utexas.edu>
2024-10-31 23:56:36 -0500

    Add sponsor tracks, add food menu, add UTCS sponsor

--- current/src/prizes.js
+++ current/src/prizes.js
@@ -10,6 +10,7 @@ function createPrizeCard(title, desc) {
 function populatePrizes() {
   const normalTrack = document.getElementById("normal-tracks");
   const AITrack = document.getElementById("AI-tracks");
+  const sponsorTrack = document.getElementById("sponsor-tracks");
 
   const normalData = [
     {
@@ -42,16 +43,19 @@ function populatePrizes() {
     {
       t: "BEST GROUND-UP MODEL",
       d: "We’ve all used pre-trained models and created a good use out of them, but what about actually training a model for your purpose? This award goes to the hackers that train a model from the ground-up that has a great use case and creative parameters.",
-    },
+    }
+  ];
+
+  const sponsorData = [
     {
       t: "Best use of GenAI using InterSystems IRIS Vector Search",
-      d: "This award celebrates the most innovative application of Generative AI utilizing InterSystems IRIS Vector Search. Judges will evaluate entries based on the effectiveness and creativity of GenAI integration, the complexity and precision of the vector search solution, and the real-world impact and potential for scaling this technology. For more information, check out <a href='/https://developer.intersystems.com/intersystems-genai-challenge-hacktx-2024/'> here </a>!",
+      d: "This award celebrates the most innovative application of Generative AI utilizing InterSystems IRIS Vector Search. Judges will evaluate entries based on the effectiveness and creativity of GenAI integration, the complexity and precision of the vector search solution, and the real-world impact and potential for scaling this technology. For more information, check out <a href='https://developer.intersystems.com/intersystems-genai-challenge-hacktx-2024/'> here </a>!",
     },
     {
       t: "Best Use of Intel AI",
       d: "Unleash your creativity and build cutting-edge AI applications with Intel-optimized software on the latest Intel hardware including Intel Tiber Developer Cloud and AI PC Dev Kits.Tackle real-world challenges using Intel technology in your hackathon project for a chance to win awesome prizes. Join the Best Use of Intel AI track and demonstrate the future of AI innovation.",
     },
-  ];
+  ]
 
   normalData.forEach((item) => {
     normalTrack.innerHTML += createPrizeCard(item.t, item.d);
@@ -59,6 +63,9 @@ function populatePrizes() {
   AIData.forEach((item) => {
     AITrack.innerHTML += createPrizeCard(item.t, item.d);
   });
+  sponsorData.forEach((item) => {
+    sponsorTrack.innerHTML += createPrizeCard(item.t, item.d);
+  })
 }
 
 populatePrizes();
