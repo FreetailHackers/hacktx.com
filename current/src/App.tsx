163aabe7ed09b19cce93381b894027148a358a69
nikshitak <nikshitak@utexas.edu>
2025-06-26 13:21:48 -0500

    Revert "fixed asset imports"
    This reverts commit e99405e6328f875b0ec72cf85bb2f82a52120cf4.

--- current/src/App.tsx
+++ current/src/App.tsx
@@ -1,9 +1,4 @@
 import React from "react";
-import freetailLogo from "./assets/freetail_logo.svg";
-import interestFormButton from "./assets/interest_form.svg";
-import starsBackground from "./assets/stars_bg.svg";
-import heading from "./assets/heading.svg";
-import MLHBadge from "./assets/mlh_badge.svg";
 
 function App() {
   return (
@@ -13,14 +8,14 @@ function App() {
           <a href="https://freetailhackers.com/" target="_blank">
             <img
               className="relative top-3 left-3 size-9"
-              src={freetailLogo}
+              src="/freetail_logo.png"
             />
           </a>
 
           <a href="https://mlh.io/" target="_blank">
             <img
               className="relative h-[144px] sm:h-auto xl:right-20"
-              src={MLHBadge}
+              src="/mlh_badge.svg"
             />
           </a>
         </div>
@@ -30,14 +25,14 @@ function App() {
           <div>
             <img
               className="w-full max-w-[56.25rem] min-w-[22.5rem]"
-              src={heading}
+              src="/heading.svg"
             />
           </div>
 
           <p className="mb-5 text-2xl uppercase sm:text-3xl">MID OCTOBER</p>
 
           <button className="transition-shadow hover:cursor-pointer hover:shadow-[0px_0px_14px] hover:shadow-[#DDB945]">
-            <img src={interestFormButton}/>
+            <img src="/interest_form.svg " />
           </button>
         </div>
 
@@ -55,7 +50,7 @@ function App() {
       </main>
 
       <img
-        src={starsBackground}
+        src="/stars_bg.svg"
         className="absolute inset-0 size-full object-cover"
         alt="Background Stars"
       />
