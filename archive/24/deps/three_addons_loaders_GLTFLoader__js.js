10aa1e181efca93785a58b62d864968ae6b22b25
nikshitak <nikshitak@utexas.edu>
2025-07-20 22:23:09 -0500

    moved everything up a directory, hopefully hacktx 24 works

---
+++ archive/24/deps/three_addons_loaders_GLTFLoader__js.js
@@ -0,0 +1,10 @@
+import {
+  GLTFLoader
+} from "./chunk-W3YJ3MUM.js";
+import "./chunk-GJZHIFXU.js";
+import "./chunk-RZ4XFWCU.js";
+import "./chunk-V4OQ3NZ2.js";
+export {
+  GLTFLoader
+};
+//# sourceMappingURL=three_addons_loaders_GLTFLoader__js.js.map
---
+++ archive/24/deps/three_examples_jsm_utils_BufferGeometryUtils__js.js
@@ -0,0 +1,33 @@
+import {
+  computeMikkTSpaceTangents,
+  computeMorphedAttributes,
+  deepCloneAttribute,
+  deinterleaveAttribute,
+  deinterleaveGeometry,
+  estimateBytesUsed,
+  interleaveAttributes,
+  mergeAttributes,
+  mergeGeometries,
+  mergeGroups,
+  mergeVertices,
+  toCreasedNormals,
+  toTrianglesDrawMode
+} from "./chunk-GJZHIFXU.js";
+import "./chunk-RZ4XFWCU.js";
+import "./chunk-V4OQ3NZ2.js";
+export {
+  computeMikkTSpaceTangents,
+  computeMorphedAttributes,
+  deepCloneAttribute,
+  deinterleaveAttribute,
+  deinterleaveGeometry,
+  estimateBytesUsed,
+  interleaveAttributes,
+  mergeAttributes,
+  mergeGeometries,
+  mergeGroups,
+  mergeVertices,
+  toCreasedNormals,
+  toTrianglesDrawMode
+};
+//# sourceMappingURL=three_examples_jsm_utils_BufferGeometryUtils__js.js.map
