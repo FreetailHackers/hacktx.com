f48a152b32c3fc8423918c144f9494ae977d3def
khang <khang.h.t@utexas.edu>
2025-08-24 16:22:47 -0500

    Landing section

--- current/src/components/Navbar.tsx
+++ current/src/components/Navbar.tsx
@@ -2,7 +2,13 @@ import React from "react";
 
 import Button from "./Button";
 
-function NavbarLink({ href, children }: { href: string; children: React.ReactNode }) {
+function NavbarLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
   return (
     <a href={href} className="text-lg hover:underline">
       {children}
@@ -12,40 +18,32 @@ function NavbarLink({ href, children }: { href: string; children: React.ReactNod
 
 export default function Navbar() {
   return (
-    <nav className="relative z-50 max-w-7xl p-5 mt-6 mx-auto flex bg-[#000C4446] justify-between items-center">
-      <a href="https://freetailhackers.com/" target="_blank">
-        <img
-          className="h-[72px] sm:h-auto hover:drop-shadow-[0_0_4px] transition-all duration-300 ease-out"
-          src="/images/HackTX25_logo.png"
-          alt="Freetail Hackers Logo"
-        />
-      </a>
+    <nav className="fixed z-50 top-0 left-0 right-0">
+      <div className="relative max-w-7xl h-[76px] mt-6 mx-auto px-3 flex justify-between items-center bg-[#000C4446]">
+        <a href="https://freetailhackers.com/" target="_blank">
+          <img
+            className="h-[72px] sm:h-auto hover:drop-shadow-[0_0_4px] transition-all duration-300 ease-out"
+            src="/images/HackTX25_logo.png"
+            alt="Freetail Hackers Logo"
+          />
+        </a>
 
-      <div className="flex gap-8">
-        <NavbarLink href="#about">
-          About
-        </NavbarLink>
-        <NavbarLink href="#about">
-          Schedule
-        </NavbarLink>
-        <NavbarLink href="#about">
-          Tracks
-        </NavbarLink>
-        <NavbarLink href="#about">
-          FAQ
-        </NavbarLink>
+        <div className="flex gap-8">
+          <NavbarLink href="#about">About</NavbarLink>
+          <NavbarLink href="#schedule">Schedule</NavbarLink>
+          <NavbarLink href="#tracks">Tracks</NavbarLink>
+          <NavbarLink href="#faq">FAQ</NavbarLink>
 
-        <Button href="https://forms.gle/SahZJw8p7s1gb7yEA">
-          Apply
-        </Button>
+          <Button href="https://forms.gle/SahZJw8p7s1gb7yEA">Apply</Button>
+        </div>
+
+        <a href="https://mlh.io/" target="_blank">
+          <img
+            className="absolute h-[144px] sm:h-auto xl:right-6 top-3"
+            src="/mlh_badge.svg"
+          />
+        </a>
       </div>
-
-      <a href="https://mlh.io/" target="_blank">
-        <img
-          className="absolute h-[144px] sm:h-auto xl:right-6 top-3"
-          src="/mlh_badge.svg"
-        />
-      </a>
     </nav>
-  )
+  );
 }
---
+++ current/src/sections/About.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+function Tag({ children }: { children: React.ReactNode }) {
+  return (
+    <span className="px-2 py-1 text-yellow ring-1 ring-yellow text-blue rounded-full text-sm font-semibold">
+      {children}
+    </span>
+  );
+}
+
+export default function About() {
+  return (
+    <section className="relative w-screen">
+      <img
+        src="/vectors/Landing Stars.svg"
+        alt="Landing Background"
+        className="absolute -top-32 left-1/2 -translate-x-1/2 w-[1605px] object-fill select-none pointer-events-none"
+        draggable={false}
+      />
+
+      <div className="mx-auto flex flex-row w-screen max-w-[1200px] justify-between">
+        <img
+          src="/images/Splash.png"
+          alt="HackTX 2025 Logo"
+          className="select-none"
+          draggable={false}
+        />
+
+        <div className="flex flex-col justify-center items-end">
+          <div className="flex gap-5 justify-center mb-8">
+            <Tag>Mid-October</Tag>
+            <Tag>Austin, TX</Tag>
+            <Tag>24 Hours</Tag>
+          </div>
+
+          <img
+            src="/images/HackTX25_title.png"
+            alt="HackTX 25"
+            className="max-h-[263px] select-none"
+            draggable={false}
+          />
+
+          <div className="text-right text-base text-white mt-0.5">
+            Applications open August 25th at 9:00 AM CDT
+            <br />
+            Interested in sponsoring us?{" "}
+            <a
+              href="mailto:corporate@freetailhackers.com"
+              className="underline"
+            >
+              Learn More
+            </a>
+          </div>
+
+          <a href="https://forms.gle/SahZJw8p7s1gb7yEA" className="group mt-6">
+            <img src="/vectors/Fancy Button.svg" alt="Apply Now" className="group-hover:drop-shadow-[0_0_3px] transition-all ease-out"/>
+            {/* <img src="/vectors/Fancy Button.svg" alt="Apply Now" className="group-hover:hidden"/>
+            <img src="/vectors/Fancy Button Glow.svg" alt="Apply Now" className="hidden group-hover:block"/> */}
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
