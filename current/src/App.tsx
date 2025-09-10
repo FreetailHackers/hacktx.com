7d84999391ad21ad8f127923d3aa8e93e1051265
Ethan Lanting <ethanlanting@gmail.com>
2025-09-10 20:50:10 +0000

    Fix FAQ section

--- current/src/App.tsx
+++ current/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <About />
         <FAQ />
         <Footer />
-        <img src="/images/Nebula.png" alt="Nebula" className="absolute -z-10 -bottom-96 left-1/2 -translate-x-1/2 w-[1520px]" />
+        <img src="/images/Nebula.png" alt="Nebula" className="absolute -z-10 top-[600px] left-1/2 -translate-x-1/2 w-[1520px]" />
       </main>
     </>
   );
