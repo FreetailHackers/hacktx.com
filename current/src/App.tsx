19f7be676e948248f5f725f996413a8efcd4b12e
Ethan Lanting <ethanlanting@gmail.com>
2025-09-10 03:51:00 +0000

    Add faq section

--- current/src/App.tsx
+++ current/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar"
 import About from "./sections/About";
+import FAQ from "./sections/FAQ";
 import Footer from "./sections/Footer";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <main className="relative z-10 flex min-h-screen flex-col pt-28 px-5 overflow-clip">
         <About />
+        <FAQ />
         <Footer />
         <img src="/images/Nebula.png" alt="Nebula" className="absolute -z-10 -bottom-96 left-1/2 -translate-x-1/2 w-[1520px]" />
       </main>
