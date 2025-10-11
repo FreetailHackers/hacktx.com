f207c2de5e9217c16902e005af861861c913282f
khang <khang.h.t@utexas.edu>
2025-10-11 17:21:14 -0500

    Add Freetail Constellation, Socials links, polish footer, polish about section, font changes for small, add FAQ to navbar

--- current/src/components/Navbar.tsx
+++ current/src/components/Navbar.tsx
@@ -27,8 +27,8 @@ function Button({ href, children }: { href: string; children: React.ReactNode })
 export default function Navbar() {
   return (
     <nav className="fixed max-lg:absolute z-50 top-0 left-0 right-0">
-      <div className="relative max-w-6xl h-[76px] mt-6 mx-auto flex justify-between items-center lg:bg-[#000C44B2]">
-        <a href="https://freetailhackers.com/" target="_blank">
+      <div className="relative max-w-6xl h-[76px] mt-6 mx-auto flex justify-center items-center lg:bg-[#000C44B2]">
+        <a href="https://freetailhackers.com/" target="_blank" className="absolute left-0">
           <img
             className="h-[48px] mx-6 hover:drop-shadow-custom transition-all duration-300 ease-out"
             src="/images/HackTX25_logo.png"
@@ -38,6 +38,7 @@ export default function Navbar() {
 
         <div className="flex gap-8 max-lg:hidden">
           <NavbarLink href="#about">About</NavbarLink>
+          <NavbarLink href="#faq">FAQ</NavbarLink>
 
           <Button href="https://rodeo.freetailhackers.com/">Apply</Button>
         </div>
--- current/src/sections/Footer.tsx
+++ current/src/sections/Footer.tsx
@@ -28,8 +28,83 @@ function Link({ href, children }: { href: string; children: React.ReactNode }) {
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="flex flex-row flex-wrap justify-between max-w-7xl mx-auto w-full my-10 text-white">
+    <footer className="relative max-w-7xl w-full mx-auto">
+      <div className="absolute w-[1680px] h-[1200px] -z-10 overflow-visible">
+        <img
+          src="/images/Footer Nebula.png"
+          alt="Footer Nebula"
+          draggable={false}
+        />
+      </div>
+      <div className="absolute left-1/2 -translate-x-1/2 w-[1400px] h-[970px] top-[180px] -z-10">
+        <img
+          src="/vectors/Footer Stars.svg"
+          alt="Footer Stars"
+          draggable={false}
+          className="w-[1400px] h-[970px]"
+        />
+      </div>
+      <div className="w-min lg:absolute">
+        <h3 className="text-xl font-bold text-white flex items-center gap-2 mb-2.5">
+          <img
+            src="/vectors/FH Bat.svg"
+            alt="logo"
+            draggable={false}
+            className="inline"
+          />
+          Freetail hackers
+        </h3>
+        <div className="mb-3">
+          <a href="https://freetailhackers.com" className="text-grey">
+            freetailhackers.com
+          </a>
+          <br />
+          <a href="mailto:admin@freetailhackers.com" className="text-grey">
+            admin@freetailhackers.com
+          </a>
+        </div>
+        <div className="flex flex-row gap-3">
+          <a
+            href="https://www.linkedin.com/company/freetail-hackers"
+            className="hover:drop-shadow-custom transition-all ease-out"
+          >
+            <img src="/vectors/linkedin.svg" />
+          </a>
+          <a
+            href="https://www.instagram.com/freetailhackers"
+            className="hover:drop-shadow-custom transition-all ease-out"
+          >
+            <img src="/vectors/instagram.svg" />
+          </a>
+          <a
+            href="https://www.facebook.com/freetailhackers/ "
+            className="hover:drop-shadow-custom transition-all ease-out"
+          >
+            <img src="/vectors/facebook.svg" />
+          </a>
+          <a
+            href="https://www.tiktok.com/@freetailhackers"
+            className="hover:drop-shadow-custom transition-all ease-out"
+          >
+            <img src="/vectors/tiktok.svg" />
+          </a>
+          <a
+            href="https://x.com/freetailhackers"
+            className="hover:drop-shadow-custom transition-all ease-out"
+          >
+            <img src="/vectors/twitter.svg" />
+          </a>
+        </div>
+      </div>
+      <div>
+        <img
+          src="/images/Freetail Constellation.png"
+          alt="Freetail Constellation"
+          draggable={false}
+          className="w-full mt-10"
+        />
+      </div>
+      <div className="flex flex-row flex-wrap justify-between w-full my-10 text-white">
         <Column title="HackTX 2024">
           <Link href="https://hacktx.com/24/">Website</Link>
           <Link href="https://hacktx-2024.devpost.com/">Devpost</Link>
@@ -55,10 +130,23 @@ export default function Footer() {
         </Column>
       </div>
       <div className="py-6 w-full flex flex-row flex-wrap justify-between max-w-7xl mx-auto text-grey">
-        <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" className="underline hover:drop-shadow-custom transition-all ease-out min-w-max" >
+        <a
+          href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+          className="underline hover:drop-shadow-custom transition-all ease-out min-w-max"
+        >
           Code of Conduct
         </a>
         <div className="text-right">Copyright @ Freetail Hackers 2025</div>
+      </div>
+      <div className="relative h-[400px]">
+        <div className="absolute left-1/2 -translate-x-1/2 w-[1287px] h-[892px] -top-[400px] -z-10 overflow-visible">
+          <img
+            src="/images/Footer Stars 2.png"
+            alt="Footer Stars 2"
+            draggable={false}
+          />
+        </div>
+        <h2 className="font-serif text-5xl text-white mt-20">What's in your <br/> future?</h2>
       </div>
     </footer>
   );
