25da00589c2bed6455cc7624e5dca8e03ef75a22
khang <khang.h.t@utexas.edu>
2025-10-11 20:42:00 -0500

    Create partners section

--- current/src/App.tsx
+++ current/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar"
 import About from "./sections/About";
 import FAQ from "./sections/FAQ";
+import Partners from "./sections/Partners";
 import Footer from "./sections/Footer";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <main className="relative z-10 flex min-h-screen flex-col pt-28 px-5 overflow-clip">
         <About />
         <FAQ />
+        <Partners />
         <Footer />
       </main>
     </>
