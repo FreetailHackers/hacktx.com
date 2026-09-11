e3516c5361a7a4def800ab74b38743176519ec66
brandonshin <brandonshin@utexas.edu>
2026-09-11 14:44:14 -0500

    revert to sept 11

--- current/src/components/Landing/Tree.tsx
+++ current/src/components/Landing/Tree.tsx
@@ -116,7 +116,7 @@ export default function Tree({ className = "", style }: Props) {
           fontWeight={400}
           fill="#413720"
         >
-          Sept 15
+          Sept 11
         </text>
       </g>
 
